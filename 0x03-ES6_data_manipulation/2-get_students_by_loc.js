1b503f92fca49effd194fc36891b4eb4473abdce
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-03-01 10:12:30 +0000

    added filter function

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 const getStudentsByLocation(students, city) {
-  return students.filter((student) => students.location === city);
+	return students.filter((student) => students.location === city);
 }
 
 export default getStudentsByLocation;

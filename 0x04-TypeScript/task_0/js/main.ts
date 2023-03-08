d8db09e1e8f0c48ddf1d6e54836362c968a45d13
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-03-08 14:05:55 +0100

    added first typescript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+	firstName: string,
+	lastName: string,
+	age: Number,
+	location: string
+}
+
+const student1: Student = {
+	firstName: "Dolamu",
+	lastName: "Oludare",
+	age: 22,
+	location: "Lagos"
+}
+
+const student2: Student = {
+	firstName: "Tester",
+	lastName: "Lasisi",
+	age: 29,
+	location: "Ethopia"
+}
